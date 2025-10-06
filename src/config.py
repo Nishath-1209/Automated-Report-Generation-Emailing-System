e1d70b084ab77cf9a905d49dd87afbd2f926e774
Nishath-1209 <snishathtabassum09@gmail.com>
2025-10-06 08:50:35 +0530

    Save local changes before rebase

--- src/config.py
+++ src/config.py
@@ -6,10 +6,10 @@ SUPABASE_KEY = st.secrets["SUPABASE"]["SUPABASE_KEY"]
 
 sb: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+def get_supabase() -> Client:
+    return sb
+
 SMTP_SERVER = st.secrets["SMTP"]["SMTP_SERVER"]
 SMTP_PORT = st.secrets["SMTP"]["SMTP_PORT"]
 SMTP_USER = st.secrets["SMTP"]["SMTP_USER"]
 SMTP_PASS = st.secrets["SMTP"]["SMTP_PASS"]
-
-def get_supabase() -> Client:
-    return sb
